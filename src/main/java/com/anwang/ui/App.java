6e8340170fe989d88660890d69605d98a921bd34
lemengbin <lemengbin@163.com>
2025-06-04 16:12:14 +0800

    update warn message

--- src/main/java/com/anwang/ui/App.java
+++ src/main/java/com/anwang/ui/App.java
@@ -27,7 +27,7 @@ public class App extends JFrame {
             if (startupPage.closeType == 1) {
                 app = new App();
                 app.setVisible(true);
-                JOptionPane.showMessageDialog(app, "请注意: 当前工具将对 5月13日15:00:00 至 5月31日23:59:59 新/迁移的主节点 分发奖励，总奖励金额为: " + startupPage.amount + " SAFE");
+                JOptionPane.showMessageDialog(app, "<html>请注意: 当前工具将对 5月13日15:00:00 至 5月30日15:00:00 新/迁移的主节点 分发奖励。<br><br>总奖励金额为: " + startupPage.amount + " SAFE。<br><br>请确保私钥对应地址有足够的金额！！！");
             }
         });
     }
